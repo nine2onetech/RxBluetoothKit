2af3aabb52e562c34fbcffc24c9dc979ad586778
Simon Anreiter <simon.anreiter@wegfinder.at>
2023-06-12 12:48:44 +0200

    switch to binary-rx-swift distribution

--- Package.swift
+++ Package.swift
@@ -1,35 +1,39 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "RxBluetoothKit",
+    defaultLocalization: "en",
     platforms: [
-        .macOS(.v10_13), .iOS(.v9), .tvOS(.v11), .watchOS(.v4)
+        .macOS(.v10_13), .iOS(.v11), .tvOS(.v11), .watchOS(.v4),
     ],
     products: [
-        .library(name: "RxBluetoothKit", targets: ["RxBluetoothKit"])
+        .library(name: "RxBluetoothKit", targets: ["RxBluetoothKit"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0"))
-    ],
+    dependencies: [],
     targets: [
+        .binaryTarget(
+            name: "RxSwift",
+            url: "https://github.com/ReactiveX/RxSwift/releases/download/6.6.0/RxSwift.xcframework.zip",
+            checksum: "dd5f2f2e11a1b292d083dcbb276df5db105ff2ee4e29303f6c566796908be8b9"
+
+        ),
         .target(
-                name: "RxBluetoothKit",
-                dependencies: [
-                    "RxSwift"
-                ],
-                path: ".",
-                exclude: [
-                    "Example",
-                    "Tests",
-                    "Source/Info.plist",
-                    "Source/RxBluetoothKit.h"
-                ],
-                sources: [
-                    "Source"
-                ]
-            )
+            name: "RxBluetoothKit",
+            dependencies: [
+                "RxSwift",
+            ],
+            path: ".",
+            exclude: [
+                "Tests",
+                "Source/Info.plist",
+                "Source/RxBluetoothKit.h",
+            ],
+            sources: [
+                "Source",
+            ]
+        ),
     ]
 )
